144b1d8cc14e19432489c0fae5c061449a8b30e5
Wisdom Li <wisdom.li@zcomsystems.com>
2016-10-27 17:21:05 -0400

    update

--- createImage.php
+++ createImage.php
@@ -12,33 +12,41 @@ if ($_GET) {
 
 	$number = seperation($_GET['name']);
 
-	$chance %= 10;
+	$chance = $number % 10;
 	
 
 
-	// if ($chance <= 3){
+	if ($chance <= 3){
 		$figure = $number * 97 % 12 + 1;
 
 		$dst = resize_image("figures/n".$figure.".jpg", 150, 150, true);
 
 
-	// }else if ($chance <= 6){
+	}else if ($chance <= 6){
+		$figure = $number * 97 % 27 + 1;
 
-	// }else if ($chance <= 8){
+		$dst = resize_image("figures/r".$figure.".jpg", 150, 150, true);		
 
-	// }else if ($chance == 9){
 
-	// }else{
+	}else if ($chance <= 8){
+		$figure = $number * 97 % 16 + 1;
 
-	// }
+		$dst = resize_image("figures/sr".$figure.".jpg", 150, 150, true);
+	}else if ($chance == 9){
+		$figure = $number * 97 % 8 + 1;
+
+		$dst = resize_image("figures/ssr".$figure.".jpg", 150, 150, true);
+	}else{
+
+	}
 
 	imagealphablending($im, false);
 	imagesavealpha($im, true);
 
 	imagecopy ( $im , $dst, 389 , 50 , 0, 0 , 150 , 150 );
 
-	$font = 'font/arial.ttf';
-	// $font = 'font/cn_cute.ttf';
+	// $font = 'font/arial.ttf';
+	$font = 'font/cn_cute.ttf';
 
 	$text = $_GET['name'];
 	imagettftext($im, 65, 0, 50, 170, $color, $font, $text);
